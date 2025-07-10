677aee7429ed40a48cd328ff60a92d3cd1aeccb6
Kara Walker <hwisprian@gmail.com>
2025-07-10 14:43:50 -0500

    fix tests

--- AS3/test/test_chromosome.py
+++ AS3/test/test_chromosome.py
@@ -1,20 +1,19 @@
 import pytest
 import copy
 import chromosome
-from chromosome import Chromosome
 
 ############################################################################################
 #                              PyTests with smol order numbers
 ###########################################################################################
-chrom_5_random = Chromosome(5)
-chrom_5 = Chromosome(0)
+chrom_5_random = chromosome.Chromosome(5)
+chrom_5 = chromosome.Chromosome(0)
 chrom_5.items.append(chromosome.Item(0, 0, 1.2))
 chrom_5.items.append(chromosome.Item(1, 1, .8))
 chrom_5.items.append(chromosome.Item(2, 2, .9))
 chrom_5.items.append(chromosome.Item(3, 3, .2))
 chrom_5.items.append(chromosome.Item(4, 4, 1.6))
 
-chrom_3 = Chromosome(0)
+chrom_3 = chromosome.Chromosome(0)
 chrom_3.items.append(chromosome.Item(0, 0, 1.2))
 chrom_3.items.append(chromosome.Item(1, 1, .8))
 chrom_3.items.append(chromosome.Item(2, 2, .9))
@@ -51,7 +50,7 @@ def test_get_constraint_violation():
 def test_get_mutation_probability():
     assert chrom_5.get_mutation_probability() == .2 # has 5 boxes!
     assert chrom_5_random.get_mutation_probability() >= .2 # will have 5 or less boxes.
-    assert Chromosome(100).get_mutation_probability() >= .01  # will have 100 or less boxes.
+    assert chromosome.Chromosome(100).get_mutation_probability() >= .01  # will have 100 or less boxes.
 
 def test_tournament_selection():
     assert chromosome.tournament_selection(chrom_5, chrom_3) == chrom_3
@@ -78,7 +77,7 @@ def test_tournament_selection():
 
 def test_crossover():
     test_chrom = copy.deepcopy(chrom_5)
-    test_chrom2 = Chromosome(5)
+    test_chrom2 = chromosome.Chromosome(5)
     offspring = test_chrom.crossover(test_chrom2, 1, 0)
     print("Crossover Offspring:", offspring)
     assert test_chrom.bin_assignments != chrom_5.bin_assignments
